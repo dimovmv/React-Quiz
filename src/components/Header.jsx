decc1b53e2a61e65cc2d827d1474d3045ee2cdd4
dimovmv <dimodimov.mv@gmail.com>
2024-05-14 19:24:19 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className='app-header'>
+      <img src='src/assets/react.svg' alt='React logo' />
+      <h1>React Quiz</h1>
+    </header>
+  );
+}
